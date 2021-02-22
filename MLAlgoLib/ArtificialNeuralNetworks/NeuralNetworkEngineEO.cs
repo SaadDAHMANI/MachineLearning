698b37c312ea5a7973c5ef304ac2bdc69762e290
Saad DAHMANI <TahatAtakor7@gmail.com>
2021-02-22 21:39:39 +0100

    Add Evolutionary ANN class.
    Signed-off-by: Saad DAHMANI <TahatAtakor7@gmail.com>

---
+++ MLAlgoLib/ArtificialNeuralNetworks/NeuralNetworkEngineEO.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Accord.Neuro;
+using Accord.Neuro.Learning;
+using IOOperations;
+
+namespace MLAlgoLib
+{
+namespace ArtificialNeuralNetwork
+{
+
+ [Serializable]
+        public class NeuralNetworkEngineEO
+        {
+
+
+
+        }
+
+}
+}
